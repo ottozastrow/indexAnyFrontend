a1e8d179ae8eaed06d277b0d6be6dae3005f7a6f
Otto Zastrow <otto.zastrow@hotmail.de>
2022-08-08 02:12:22 +0200

    minors

--- standlet/docusaurus.config.js
+++ standlet/docusaurus.config.js
@@ -64,16 +64,17 @@ const config = {
         // Optional: see doc section below
         contextualSearch: true,
   
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: 'external\\.com|domain\\.com',
-  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.  
         // Optional: Algolia search parameters
-        searchParameters: {},
+        searchParameters: {
+          hitsPerPage: 10,
+        },
   
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
   
         //... other Algolia params
+
       },
       navbar: {
         title: 'standlet',
